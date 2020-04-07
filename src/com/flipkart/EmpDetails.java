fb149d0274f58a016e9cfc2ecb9557fb17291aa8
KUMARan429 <KUMARAN6143@gmail.com>
2020-04-07 12:13:46 +0530

    Code Assigned As per the Requirement

--- src/com/flipkart/EmpDetails.java
+++ src/com/flipkart/EmpDetails.java
@@ -5,7 +5,8 @@ public class EmpDetails {
 public static void main(String[] args) {
 	EmpDetails e= new EmpDetails();
 	e.hashCode();
-	
+	System.out.println("Mission");
+	System.out.println("FirstProject done");
 }}
 		
 		
