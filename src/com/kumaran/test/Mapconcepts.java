f5ee5a36e8c7cb9712fdbde477a35eff04697005
KUMARan429 <KUMARAN6143@gmail.com>
2020-04-07 11:29:51 +0530

    FirstDay

--- src/com/kumaran/test/Mapconcepts.java
+++ src/com/kumaran/test/Mapconcepts.java
@@ -5,22 +5,6 @@ import java.util.Map;
 
 public class Mapconcepts {
 public static void main(String[] args) {
-	String s="HELLOKUMARANWHEREAREYOUNOW";
-	char[] ch = s.toCharArray();
-	Map<Character, Integer> m=new HashMap<Character, Integer>();
-	for (char c : ch) {
-		if (m.containsKey(c)) {
-			Integer i = m.get(c);
-			m.put(c, i+1);
-		}else {
-			m.put(c, 1);
-		}
-		
-	}
-	System.out.println(m);
-	
-	
-	
 	
 	
 	
